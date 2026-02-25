10f5d8fcd859b5d1b5142b6ebf597c0d299fb3e4
aryanyadavcoder <aryan9250yadav@gmail.com>
2026-02-25 13:38:24 +0530

    hii

--- result.py
+++ result.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-response = requests.get("https://raw.githubusercontent.com/aryanyadavcoder/lerning_JSON/refs/heads/main/result1.json")
+response = requests.get("https://aryanyadavcoder.github.io/lerning_JSON/result1.json")
 data =str( response.json())
-# data=json.loads(data)
+data=json.loads(data)
 print(data)
 # for key,value in data.items():
 #     print(key,value)
