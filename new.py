21a1a85a3e7672a3cc5caf201653dfcb7388d334
aryanyadavcoder <aryan9250yadav@gmail.com>
2026-02-25 11:37:42 +0530

    new

--- new.py
+++ new.py
@@ -6,3 +6,9 @@ response = requests.get("https://raw.githubusercontent.com/aryanyadavcoder/lerni
 data = response.json()
 print(data)
 
+
+ 
+
+response = requests.get("https://raw.githubusercontent.com/aryanyadavcoder/lerning_JSON/refs/heads/main/data1.json")
+data = response.json()
+print(data)
