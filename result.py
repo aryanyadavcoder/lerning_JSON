32b0e011225572a453034ad7d3022c2cd7328678
aryanyadavcoder <aryan9250yadav@gmail.com>
2026-02-25 14:13:53 +0530

    new

--- result.py
+++ result.py
@@ -3,10 +3,11 @@ import json
 
 response = requests.get("https://raw.githubusercontent.com/aryanyadavcoder/lerning_JSON/refs/heads/main/result1.json")
 # data='[{"name":"amit","rollno":45,"math":65,"englih":56},{"name":"rohit","rollno":40,"math":55,"englih":46}]'
-data =str( response.json())
+data =( response.json())
+# print(type(data))
 # data=json.loads(data)
-print(data)
-# for key,value in data.items():
-#     print(key,value)
+# print(data)
+for key,value in data.items():
+    print(key,value)
 
-print(data)
+# print(data)
