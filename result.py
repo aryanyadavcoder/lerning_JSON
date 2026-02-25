88e19530a6f62c46b2b41edf57d7c00f92226768
aryanyadavcoder <aryan9250yadav@gmail.com>
2026-02-25 14:05:28 +0530

    Update result.py

--- result.py
+++ result.py
@@ -1,8 +1,8 @@
 import requests
 import json
 
-response = requests.get("https://aryanyadavcoder.github.io/lerning_JSON/result1.json")
-data=[{"name":"amit","rollno":45,"math":65,"englih":56},{"name":"rohit","rollno":40,"math":55,"englih":46}]
+response = requests.get("https://raw.githubusercontent.com/aryanyadavcoder/lerning_JSON/refs/heads/main/result1.json")
+# data='[{"name":"amit","rollno":45,"math":65,"englih":56},{"name":"rohit","rollno":40,"math":55,"englih":46}]'
 data =str( response.json())
 # data=json.loads(data)
 print(data)
